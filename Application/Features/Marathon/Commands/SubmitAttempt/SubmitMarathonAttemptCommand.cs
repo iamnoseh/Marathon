32f159cf9f57141d72930502c62d19269e30e290
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-12-24 13:38:54 +0500

    .

--- Application/Features/Marathon/Commands/SubmitAttempt/SubmitMarathonAttemptCommand.cs
+++ Application/Features/Marathon/Commands/SubmitAttempt/SubmitMarathonAttemptCommand.cs
@@ -1,12 +1,15 @@
 using Application.DTOs;
 using Application.Responses;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace Application.Features.Marathon.Commands.SubmitAttempt;
 
 public class SubmitMarathonAttemptCommand : IRequest<Response<BestResultDto>>
 {
+    [JsonIgnore]
     public string UserId { get; set; } = string.Empty;
+    
     public int FrontendScore { get; set; }
     public int BackendScore { get; set; }
 }
