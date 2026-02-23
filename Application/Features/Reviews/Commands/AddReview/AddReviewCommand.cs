e1d988987b4bf8d53fb16abc2549af5f34e6fedd
Noseh Taghaymurodzoda <noseh.tagaymurodzoda@gmail.com>
2026-02-23 15:59:17 +0500

    fixed

--- Application/Features/Reviews/Commands/AddReview/AddReviewCommand.cs
+++ Application/Features/Reviews/Commands/AddReview/AddReviewCommand.cs
@@ -1,10 +1,12 @@
 using Application.Responses;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace Application.Features.Reviews.Commands.AddReview;
 
 public class AddReviewCommand : IRequest<Response<int>>
 {
+    [JsonIgnore]
     public string UserId { get; set; } = string.Empty;
     public string Text { get; set; } = string.Empty;
 }
