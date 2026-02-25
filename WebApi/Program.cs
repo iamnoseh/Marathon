04c2cdb05fc0ce1af01ec39b45564c1fd5097988
Noseh Taghaymurodzoda <noseh.tagaymurodzoda@gmail.com>
2026-02-25 16:49:29 +0500

    .

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
